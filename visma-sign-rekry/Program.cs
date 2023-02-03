9e4226d7b5bf29484247ee19bd8ff9844e692ebb
Eelis Mikkola <eelis@finvoicer.fi>
2023-02-03 11:39:10 +0200

    Init project

---
+++ visma-sign-rekry/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace visma_sign_rekry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO:
+            // - ArgumentProvider
+            // - Parser / input string format
+            //   - Input string validation
+            // - Validation
+            // - Calculation
+
+            // + UNIT TESTS!
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
