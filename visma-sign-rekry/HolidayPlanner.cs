4d3972421b7b493f0e7660cc52f5724b483f6ee6
Eelis Mikkola <eelis@finvoicer.fi>
2023-02-03 11:41:44 +0200

    Add holiday planner skeleton class

---
+++ visma-sign-rekry/HolidayPlanner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visma_sign_rekry
+{
+    public class HolidayPlanner : IHolidayPlanner
+    {
+        private readonly INationalHolidayProvider _nationalHolidayProvider;
+
+
+        public HolidayPlanner(INationalHolidayProvider nationalHolidayProvider)
+        {
+            _nationalHolidayProvider = nationalHolidayProvider;
+        }
+
+
+        public int CalculateSpentHolidays(DateTime startDate, DateTime endDate)
+        {
+            // validate
+
+            // calculate
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ visma-sign-rekry/IHolidayPlanner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visma_sign_rekry
+{
+    public interface IHolidayPlanner
+    {
+        // Should there be method(s) that take string arguments
+
+        // Should there be a method that returns a list of dates that use up vacation days (instead of just the number of them)
+        int CalculateSpentHolidays(DateTime startDate, DateTime endDate);
+
+    }
+}
