61a2e2b51a9a788a0402900441ab33f4ae367232
Eelis Mikkola <eelis@finvoicer.fi>
2023-02-03 11:42:46 +0200

    Add skeleton class of finnish national holiday provider

---
+++ visma-sign-rekry/FinnishNationalHolidayProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visma_sign_rekry
+{
+    public class FinnishNationalHolidayProvider : INationalHolidayProvider
+    {
+        public string Locale { get => "FI"; }
+
+
+        public List<DateTime> GetNationalHolidays(DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ visma-sign-rekry/INationalHolidayProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visma_sign_rekry
+{
+    public interface INationalHolidayProvider
+    {
+        string Locale { get; }
+
+        List<DateTime> GetNationalHolidays(DateTime startDate, DateTime endDate);
+    }
+}
